9b364238208c115d598aa57d65db19d48d884c2f
paucp <paucanpin@gmail.com>
2018-05-15 20:11:49 +0200

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFontEditor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAccept = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonAccept.Enabled = false;
+            this.buttonAccept.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonAccept.FlatAppearance.BorderSize = 0;
+            this.buttonAccept.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.buttonAccept.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.buttonAccept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAccept.Font = new System.Drawing.Font("Open Sans SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAccept.Location = new System.Drawing.Point(192, 67);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(91, 42);
+            this.buttonAccept.TabIndex = 0;
+            this.buttonAccept.Text = "Set font";
+            this.buttonAccept.UseVisualStyleBackColor = false;
+            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto Mono", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Font:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto Mono", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "None";
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonSelect.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonSelect.FlatAppearance.BorderSize = 0;
+            this.buttonSelect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.buttonSelect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.buttonSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelect.Font = new System.Drawing.Font("Open Sans SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelect.Location = new System.Drawing.Point(89, 67);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(97, 42);
+            this.buttonSelect.TabIndex = 4;
+            this.buttonSelect.Text = "Select font";
+            this.buttonSelect.UseVisualStyleBackColor = false;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(295, 121);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAccept);
+            this.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WindowsFontEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAccept;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSelect;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WindowsFontEditor
+{
+    public partial class Main : Form
+    {
+        private const string registryAdress = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes";
+        private Font selectedFont;
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.MinSize = 11;
+            fd.MaxSize = 11;
+            fd.FontMustExist = true;          
+            fd.ShowDialog();
+            if (fd.Font != null)
+            {
+                label2.Text = fd.Font.Name;
+                label2.Font = fd.Font;
+                selectedFont = fd.Font;
+                buttonAccept.Enabled = true;
+            }
+            else buttonAccept.Enabled = false;
+        }
+
+        private void buttonAccept_Click(object sender, EventArgs e)
+        {
+            Registry.SetValue(registryAdress, "Segoe UI", selectedFont.Name);
+            buttonAccept.Enabled = false;
+            MessageBox.Show("Font set to: " + selectedFont.Name, "Font set", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
