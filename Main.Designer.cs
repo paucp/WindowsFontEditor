800796686f10268f8042c544b6e79981564f94a1
paucp <paucanpin@gmail.com>
2018-05-15 20:37:39 +0200

    Program changes
    Registry tweak no longer necessary

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,45 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonAccept = new System.Windows.Forms.Button();
+            this.buttonSet = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonTweak = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // buttonAccept
+            // buttonSet
             // 
-            this.buttonAccept.BackColor = System.Drawing.Color.Gainsboro;
-            this.buttonAccept.Enabled = false;
-            this.buttonAccept.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.buttonAccept.FlatAppearance.BorderSize = 0;
-            this.buttonAccept.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
-            this.buttonAccept.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
-            this.buttonAccept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonAccept.Font = new System.Drawing.Font("Open Sans SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonAccept.Location = new System.Drawing.Point(192, 67);
-            this.buttonAccept.Name = "buttonAccept";
-            this.buttonAccept.Size = new System.Drawing.Size(91, 42);
-            this.buttonAccept.TabIndex = 0;
-            this.buttonAccept.Text = "Set font";
-            this.buttonAccept.UseVisualStyleBackColor = false;
-            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+            this.buttonSet.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonSet.Enabled = false;
+            this.buttonSet.FlatAppearance.BorderSize = 0;
+            this.buttonSet.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSet.Location = new System.Drawing.Point(118, 102);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(97, 34);
+            this.buttonSet.TabIndex = 0;
+            this.buttonSet.Text = "Set font";
+            this.buttonSet.UseVisualStyleBackColor = false;
+            this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Roboto Mono", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.Size = new System.Drawing.Size(36, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Font:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Roboto Mono", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(72, 28);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 20);
             this.label2.TabIndex = 3;
@@ -75,31 +73,57 @@
             // buttonSelect
             // 
             this.buttonSelect.BackColor = System.Drawing.Color.Gainsboro;
-            this.buttonSelect.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.buttonSelect.FlatAppearance.BorderSize = 0;
-            this.buttonSelect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
-            this.buttonSelect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
-            this.buttonSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonSelect.Font = new System.Drawing.Font("Open Sans SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSelect.Location = new System.Drawing.Point(89, 67);
+            this.buttonSelect.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelect.Location = new System.Drawing.Point(15, 102);
             this.buttonSelect.Name = "buttonSelect";
-            this.buttonSelect.Size = new System.Drawing.Size(97, 42);
+            this.buttonSelect.Size = new System.Drawing.Size(97, 34);
             this.buttonSelect.TabIndex = 4;
             this.buttonSelect.Text = "Select font";
             this.buttonSelect.UseVisualStyleBackColor = false;
             this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
             // 
+            // buttonTweak
+            // 
+            this.buttonTweak.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonTweak.Enabled = false;
+            this.buttonTweak.FlatAppearance.BorderSize = 0;
+            this.buttonTweak.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTweak.Location = new System.Drawing.Point(15, 62);
+            this.buttonTweak.Name = "buttonTweak";
+            this.buttonTweak.Size = new System.Drawing.Size(97, 34);
+            this.buttonTweak.TabIndex = 5;
+            this.buttonTweak.Text = "Tweak";
+            this.buttonTweak.UseVisualStyleBackColor = false;
+            this.buttonTweak.Click += new System.EventHandler(this.buttonTweak_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonReset.Enabled = false;
+            this.buttonReset.FlatAppearance.BorderSize = 0;
+            this.buttonReset.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.Location = new System.Drawing.Point(118, 62);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(97, 34);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(295, 121);
+            this.ClientSize = new System.Drawing.Size(228, 147);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonTweak);
             this.Controls.Add(this.buttonSelect);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.buttonAccept);
-            this.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Controls.Add(this.buttonSet);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Main";
@@ -112,10 +136,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button buttonAccept;
+        private System.Windows.Forms.Button buttonSet;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Button buttonTweak;
+        private System.Windows.Forms.Button buttonReset;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -1,40 +1,91 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
-using Microsoft.Win32;
 
 namespace WindowsFontEditor
 {
     public partial class Main : Form
     {
-        private const string registryAdress = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes";
+        private const string fontSubsituteAdress = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes";
+        private const string fontAdress = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts";
         private Font selectedFont;
+
         public Main()
         {
             InitializeComponent();
+            buttonReset.Enabled = IsTweaked();
+            buttonTweak.Enabled = !IsTweaked();      
+            buttonSelect.Enabled = IsTweaked();         
         }
+        private bool IsTweaked()       
+          => string.IsNullOrEmpty(Registry.GetValue(fontAdress, "Segoe UI (TrueType)", null).ToString());        
+
+        private void SetFontTweak()
+        {
+            Registry.SetValue(fontAdress, "Segoe UI (TrueType)", "");
+            Registry.SetValue(fontAdress, "Segoe UI Bold (TrueType)", "");
+            Registry.SetValue(fontAdress, "Segoe UI Bold Italic (TrueType)", "");
+            Registry.SetValue(fontAdress, "Segoe UI Italic (TrueType)", "");
+            Registry.SetValue(fontAdress, "Segoe UI Light (TrueType)", "");
+            Registry.SetValue(fontAdress, "Segoe UI Semibold (TrueType)", "");
+            Registry.SetValue(fontAdress, "Segoe UI Light (TrueType)", "");
+            Registry.SetValue(fontAdress, "Segoe UI Symbol (TrueType)", "");
+        }
+        private void ResetFontTweak()
+        {
+            Registry.SetValue(fontAdress, "Segoe UI (TrueType)", "segoeui.ttf");
+            Registry.SetValue(fontAdress, "Segoe UI Bold (TrueType)", "segoeuib.ttf");
+            Registry.SetValue(fontAdress, "Segoe UI Bold Italic (TrueType)", "segoeuiz.ttf");
+            Registry.SetValue(fontAdress, "Segoe UI Italic (TrueType)", "segoeuii.ttf");
+            Registry.SetValue(fontAdress, "Segoe UI Light (TrueType)", "segoeuil.ttf");
+            Registry.SetValue(fontAdress, "Segoe UI Semibold (TrueType)", "seguisb.ttf");       
+            Registry.SetValue(fontAdress, "Segoe UI Symbol (TrueType)", "seguisym.ttf");
+            Registry.SetValue(fontSubsituteAdress, "Segoe UI", "Segoe UI");
+        }
+        private void ChangeFont(string fontname)        
+            => Registry.SetValue(fontSubsituteAdress, "Segoe UI", selectedFont.Name);
+
+        
         private void buttonSelect_Click(object sender, EventArgs e)
         {
-            FontDialog fd = new FontDialog();
-            fd.MinSize = 11;
-            fd.MaxSize = 11;
-            fd.FontMustExist = true;          
-            fd.ShowDialog();
-            if (fd.Font != null)
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.MinSize = 11;
+            fontDialog.MaxSize = 11;
+            fontDialog.FontMustExist = true;         
+            fontDialog.ShowDialog();
+            if (fontDialog.Font != null)
             {
-                label2.Text = fd.Font.Name;
-                label2.Font = fd.Font;
-                selectedFont = fd.Font;
-                buttonAccept.Enabled = true;
+                label2.Text = fontDialog.Font.Name;
+                label2.Font = fontDialog.Font;
+                selectedFont = fontDialog.Font;
+                buttonSet.Enabled = true;
             }
-            else buttonAccept.Enabled = false;
+            else buttonSet.Enabled = false;
         }
 
-        private void buttonAccept_Click(object sender, EventArgs e)
-        {
-            Registry.SetValue(registryAdress, "Segoe UI", selectedFont.Name);
-            buttonAccept.Enabled = false;
+        private void buttonSet_Click(object sender, EventArgs e)
+        {            
+            buttonSet.Enabled = false;
+            ChangeFont(selectedFont.Name);
             MessageBox.Show("Font set to: " + selectedFont.Name, "Font set", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        private void buttonTweak_Click(object sender, EventArgs e)
+        {
+            SetFontTweak();
+            buttonTweak.Enabled = false;
+            buttonReset.Enabled = true;
+            buttonSelect.Enabled = true;           
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            ResetFontTweak();
+            buttonTweak.Enabled = true;
+            buttonReset.Enabled = false;
+            buttonSelect.Enabled = false;
+            buttonSet.Enabled = false;
+        }       
     }
 }
